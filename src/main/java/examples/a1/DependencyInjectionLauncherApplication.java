aa4d93bdbba7a33089e61dbe6ddfa3b31461250d
Anjali Rana <anjalirana7870@gmail.com>
2023-06-05 13:11:11 +0530

    added file

--- src/main/java/examples/a1/DependencyInjectionLauncherApplication.java
+++ src/main/java/examples/a1/DependencyInjectionLauncherApplication.java
@@ -9,15 +9,42 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 
-//Using Field Injection
+
 //added autowired : spring will automatically do Field Injection
 @Component
 class YourBusinessClass{
-    @Autowired
-    Dependency1 dependency1;
 
-    @Autowired
+
+    //Using constructor injection
+    Dependency1 dependency1;
     Dependency2 dependency2;
+    @Autowired
+    public YourBusinessClass(Dependency1 dependency1, Dependency2 dependency2) {
+        System.out.println("Constructor Injection - YourBusinessClass ");
+        this.dependency1 = dependency1;
+        this.dependency2 = dependency2;
+    }
+    //Using Field Injection
+    //@Autowired
+//    Dependency1 dependency1;
+//
+//    //@Autowired
+//    Dependency2 dependency2;
+
+    //using  Setter Injection
+//    @Autowired
+//
+//    public void setDependency1(Dependency1 dependency1) {
+//        System.out.println("Setter Injection - setDependency1 ");
+//        this.dependency1 = dependency1;
+//    }
+//
+//    @Autowired
+//
+//    public void setDependency2(Dependency2 dependency2) {
+//        System.out.println("Setter Injection - setDependency2 ");
+//        this.dependency2 = dependency2;
+//    }
 
     public  String toString(){
         return "Using " + dependency1 + " and " + dependency2;
